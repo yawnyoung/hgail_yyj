761d9ddf7141113a022d147682e0453e9f552a5f
yawnyoung <yajuyang-c@my.cityu.edu.hk>
2024-11-25 17:33:34 +0800

    1. formatting; 2. change np.bool to np.bool_; 3. change hero model name from mkz2017 to mkz_2017; 4. change code adaptive to carla14; 5. add spectator; 6. train; 7. trained model

--- carla_gym/core/obs_manager/birdview/chauffeurnet.py
+++ carla_gym/core/obs_manager/birdview/chauffeurnet.py
@@ -29,9 +29,9 @@ COLOR_ALUMINIUM_5 = (46, 52, 54)
 
 def tint(color, factor):
     r, g, b = color
-    r = int(r + (255-r) * factor)
-    g = int(g + (255-g) * factor)
-    b = int(b + (255-b) * factor)
+    r = int(r + (255 - r) * factor)
+    g = int(g + (255 - g) * factor)
+    b = int(b + (255 - b) * factor)
     r = min(r, 255)
     g = min(g, 255)
     b = min(b, 255)
@@ -40,12 +40,12 @@ def tint(color, factor):
 
 class ObsManager(ObsManagerBase):
     def __init__(self, obs_configs):
-        self._width = int(obs_configs['width_in_pixels'])
-        self._pixels_ev_to_bottom = obs_configs['pixels_ev_to_bottom']
-        self._pixels_per_meter = obs_configs['pixels_per_meter']
-        self._history_idx = obs_configs['history_idx']
-        self._scale_bbox = obs_configs.get('scale_bbox', True)
-        self._scale_mask_col = obs_configs.get('scale_mask_col', 1.1)
+        self._width = int(obs_configs["width_in_pixels"])
+        self._pixels_ev_to_bottom = obs_configs["pixels_ev_to_bottom"]
+        self._pixels_per_meter = obs_configs["pixels_per_meter"]
+        self._history_idx = obs_configs["history_idx"]
+        self._scale_bbox = obs_configs.get("scale_bbox", True)
+        self._scale_mask_col = obs_configs.get("scale_mask_col", 1.1)
 
         self._history_queue = deque(maxlen=20)
 
@@ -54,28 +54,42 @@ class ObsManager(ObsManagerBase):
         self._parent_actor = None
         self._world = None
 
-        self._map_dir = Path(__file__).resolve().parent / 'maps'
+        self._map_dir = Path(__file__).resolve().parent / "maps"
 
         super(ObsManager, self).__init__()
 
     def _define_obs_space(self):
         self.obs_space = spaces.Dict(
-            {'rendered': spaces.Box(
-                low=0, high=255, shape=(self._width, self._width, self._image_channels),
-                dtype=np.uint8),
-             'masks': spaces.Box(
-                low=0, high=255, shape=(self._masks_channels, self._width, self._width),
-                dtype=np.uint8)})
+            {
+                "rendered": spaces.Box(
+                    low=0,
+                    high=255,
+                    shape=(self._width, self._width, self._image_channels),
+                    dtype=np.uint8,
+                ),
+                "masks": spaces.Box(
+                    low=0,
+                    high=255,
+                    shape=(self._masks_channels, self._width, self._width),
+                    dtype=np.uint8,
+                ),
+            }
+        )
 
     def attach_ego_vehicle(self, parent_actor):
         self._parent_actor = parent_actor
         self._world = self._parent_actor.vehicle.get_world()
 
-        maps_h5_path = self._map_dir / (self._world.get_map().name + '.h5')
-        with h5py.File(maps_h5_path, 'r', libver='latest', swmr=True) as hf:
-            self._road = np.array(hf['road'], dtype=np.uint8)
-            self._lane_marking_all = np.array(hf['lane_marking_all'], dtype=np.uint8)
-            self._lane_marking_white_broken = np.array(hf['lane_marking_white_broken'], dtype=np.uint8)
+        # maps_h5_path = self._map_dir / (self._world.get_map().name + '.h5')
+        maps_h5_path = self._map_dir / (
+            self._world.get_map().name.split("/")[-1] + ".h5"
+        )
+        with h5py.File(maps_h5_path, "r", libver="latest", swmr=True) as hf:
+            self._road = np.array(hf["road"], dtype=np.uint8)
+            self._lane_marking_all = np.array(hf["lane_marking_all"], dtype=np.uint8)
+            self._lane_marking_white_broken = np.array(
+                hf["lane_marking_white_broken"], dtype=np.uint8
+            )
             # self._shoulder = np.array(hf['shoulder'], dtype=np.uint8)
             # self._parking = np.array(hf['parking'], dtype=np.uint8)
             # self._sidewalk = np.array(hf['sidewalk'], dtype=np.uint8)
@@ -83,8 +97,12 @@ class ObsManager(ObsManagerBase):
             # self._lane_marking_yellow_solid = np.array(hf['lane_marking_yellow_solid'], dtype=np.uint8)
             # self._lane_marking_white_solid = np.array(hf['lane_marking_white_solid'], dtype=np.uint8)
 
-            self._world_offset = np.array(hf.attrs['world_offset_in_meters'], dtype=np.float32)
-            assert np.isclose(self._pixels_per_meter, float(hf.attrs['pixels_per_meter']))
+            self._world_offset = np.array(
+                hf.attrs["world_offset_in_meters"], dtype=np.float32
+            )
+            assert np.isclose(
+                self._pixels_per_meter, float(hf.attrs["pixels_per_meter"])
+            )
 
         self._distance_threshold = np.ceil(self._width / self._pixels_per_meter)
         # dilate road mask, lbc draw road polygon with 10px boarder
@@ -112,19 +130,36 @@ class ObsManager(ObsManagerBase):
         snap_shot = self._world.get_snapshot()
 
         def is_within_distance(w):
-            c_distance = abs(ev_loc.x - w.location.x) < self._distance_threshold \
-                and abs(ev_loc.y - w.location.y) < self._distance_threshold \
+            c_distance = (
+                abs(ev_loc.x - w.location.x) < self._distance_threshold
+                and abs(ev_loc.y - w.location.y) < self._distance_threshold
                 and abs(ev_loc.z - w.location.z) < 8.0
-            c_ev = abs(ev_loc.x - w.location.x) < 1.0 and abs(ev_loc.y - w.location.y) < 1.0
+            )
+            c_ev = (
+                abs(ev_loc.x - w.location.x) < 1.0
+                and abs(ev_loc.y - w.location.y) < 1.0
+            )
             return c_distance and (not c_ev)
 
-        vehicle_bbox_list = self._world.get_level_bbs(carla.CityObjectLabel.Vehicles)
-        walker_bbox_list = self._world.get_level_bbs(carla.CityObjectLabel.Pedestrians)
+        # FIXME: align with the carla14 API
+        all_vehicles = self._world.get_actors().filter("vehicle.*")
+        vehicle_bbox_list = [v.bounding_box for v in all_vehicles]
+        all_pedestrians = self._world.get_actors().filter("pedestrian.*")
+        walker_bbox_list = [p.bounding_box for p in all_pedestrians]
+
+        # vehicle_bbox_list = self._world.get_level_bbs(carla.CityObjectLabel.Vehicles)
+        # walker_bbox_list = self._world.get_level_bbs(carla.CityObjectLabel.Pedestrians)
         if self._scale_bbox:
-            vehicles = self._get_surrounding_actors(vehicle_bbox_list, is_within_distance, 1.0)
-            walkers = self._get_surrounding_actors(walker_bbox_list, is_within_distance, 2.0)
+            vehicles = self._get_surrounding_actors(
+                vehicle_bbox_list, is_within_distance, 1.0
+            )
+            walkers = self._get_surrounding_actors(
+                walker_bbox_list, is_within_distance, 2.0
+            )
         else:
-            vehicles = self._get_surrounding_actors(vehicle_bbox_list, is_within_distance)
+            vehicles = self._get_surrounding_actors(
+                vehicle_bbox_list, is_within_distance
+            )
             walkers = self._get_surrounding_actors(walker_bbox_list, is_within_distance)
 
         tl_green = TrafficLightHandler.get_stopline_vtx(ev_loc, 0)
@@ -132,32 +167,59 @@ class ObsManager(ObsManagerBase):
         tl_red = TrafficLightHandler.get_stopline_vtx(ev_loc, 2)
         stops = self._get_stops(self._parent_actor.criteria_stop)
 
-        self._history_queue.append((vehicles, walkers, tl_green, tl_yellow, tl_red, stops))
+        self._history_queue.append(
+            (vehicles, walkers, tl_green, tl_yellow, tl_red, stops)
+        )
 
         M_warp = self._get_warp_transform(ev_loc, ev_rot)
 
         # objects with history
-        vehicle_masks, walker_masks, tl_green_masks, tl_yellow_masks, tl_red_masks, stop_masks \
-            = self._get_history_masks(M_warp)
+        (
+            vehicle_masks,
+            walker_masks,
+            tl_green_masks,
+            tl_yellow_masks,
+            tl_red_masks,
+            stop_masks,
+        ) = self._get_history_masks(M_warp)
 
         # road_mask, lane_mask
-        road_mask = cv.warpAffine(self._road, M_warp, (self._width, self._width)).astype(np.bool)
-        lane_mask_all = cv.warpAffine(self._lane_marking_all, M_warp, (self._width, self._width)).astype(np.bool)
-        lane_mask_broken = cv.warpAffine(self._lane_marking_white_broken, M_warp,
-                                         (self._width, self._width)).astype(np.bool)
+        road_mask = cv.warpAffine(
+            self._road, M_warp, (self._width, self._width)
+        ).astype(np.bool_)
+        lane_mask_all = cv.warpAffine(
+            self._lane_marking_all, M_warp, (self._width, self._width)
+        ).astype(np.bool_)
+        lane_mask_broken = cv.warpAffine(
+            self._lane_marking_white_broken, M_warp, (self._width, self._width)
+        ).astype(np.bool_)
 
         # route_mask
         route_mask = np.zeros([self._width, self._width], dtype=np.uint8)
-        route_in_pixel = np.array([[self._world_to_pixel(wp.transform.location)]
-                                   for wp, _ in self._parent_actor.route_plan[0:80]])
+        route_in_pixel = np.array(
+            [
+                [self._world_to_pixel(wp.transform.location)]
+                for wp, _ in self._parent_actor.route_plan[0:80]
+            ]
+        )
         route_warped = cv.transform(route_in_pixel, M_warp)
-        cv.polylines(route_mask, [np.round(route_warped).astype(np.int32)], False, 1, thickness=16)
-        route_mask = route_mask.astype(np.bool)
+        cv.polylines(
+            route_mask,
+            [np.round(route_warped).astype(np.int32)],
+            False,
+            1,
+            thickness=16,
+        )
+        route_mask = route_mask.astype(np.bool_)
 
         # ev_mask
-        ev_mask = self._get_mask_from_actor_list([(ev_transform, ev_bbox.location, ev_bbox.extent)], M_warp)
-        ev_mask_col = self._get_mask_from_actor_list([(ev_transform, ev_bbox.location,
-                                                       ev_bbox.extent*self._scale_mask_col)], M_warp)
+        ev_mask = self._get_mask_from_actor_list(
+            [(ev_transform, ev_bbox.location, ev_bbox.extent)], M_warp
+        )
+        ev_mask_col = self._get_mask_from_actor_list(
+            [(ev_transform, ev_bbox.location, ev_bbox.extent * self._scale_mask_col)],
+            M_warp,
+        )
 
         # render
         image = np.zeros([self._width, self._width, 3], dtype=np.uint8)
@@ -166,20 +228,20 @@ class ObsManager(ObsManagerBase):
         image[lane_mask_all] = COLOR_MAGENTA
         image[lane_mask_broken] = COLOR_MAGENTA_2
 
-        h_len = len(self._history_idx)-1
+        h_len = len(self._history_idx) - 1
         for i, mask in enumerate(stop_masks):
-            image[mask] = tint(COLOR_YELLOW_2, (h_len-i)*0.2)
+            image[mask] = tint(COLOR_YELLOW_2, (h_len - i) * 0.2)
         for i, mask in enumerate(tl_green_masks):
-            image[mask] = tint(COLOR_GREEN, (h_len-i)*0.2)
+            image[mask] = tint(COLOR_GREEN, (h_len - i) * 0.2)
         for i, mask in enumerate(tl_yellow_masks):
-            image[mask] = tint(COLOR_YELLOW, (h_len-i)*0.2)
+            image[mask] = tint(COLOR_YELLOW, (h_len - i) * 0.2)
         for i, mask in enumerate(tl_red_masks):
-            image[mask] = tint(COLOR_RED, (h_len-i)*0.2)
+            image[mask] = tint(COLOR_RED, (h_len - i) * 0.2)
 
         for i, mask in enumerate(vehicle_masks):
-            image[mask] = tint(COLOR_BLUE, (h_len-i)*0.2)
+            image[mask] = tint(COLOR_BLUE, (h_len - i) * 0.2)
         for i, mask in enumerate(walker_masks):
-            image[mask] = tint(COLOR_CYAN, (h_len-i)*0.2)
+            image[mask] = tint(COLOR_CYAN, (h_len - i) * 0.2)
 
         image[ev_mask] = COLOR_WHITE
         # image[obstacle_mask] = COLOR_BLUE
@@ -200,13 +262,13 @@ class ObsManager(ObsManagerBase):
             c_tl[stop_masks[i]] = 255
             c_tl_history.append(c_tl)
 
-        c_vehicle_history = [m*255 for m in vehicle_masks]
-        c_walker_history = [m*255 for m in walker_masks]
+        c_vehicle_history = [m * 255 for m in vehicle_masks]
+        c_walker_history = [m * 255 for m in walker_masks]
 
         masks = np.stack((c_road, c_route, c_lane), axis=2)
         masks = np.transpose(masks, [2, 0, 1])
 
-        obs_dict = {'rendered': image, 'masks': masks}
+        obs_dict = {"rendered": image, "masks": masks}
 
         self._parent_actor.collision_px = np.any(ev_mask_col & walker_masks[-1])
 
@@ -214,11 +276,20 @@ class ObsManager(ObsManagerBase):
 
     def _get_history_masks(self, M_warp):
         qsize = len(self._history_queue)
-        vehicle_masks, walker_masks, tl_green_masks, tl_yellow_masks, tl_red_masks, stop_masks = [], [], [], [], [], []
+        (
+            vehicle_masks,
+            walker_masks,
+            tl_green_masks,
+            tl_yellow_masks,
+            tl_red_masks,
+            stop_masks,
+        ) = ([], [], [], [], [], [])
         for idx in self._history_idx:
             idx = max(idx, -1 * qsize)
 
-            vehicles, walkers, tl_green, tl_yellow, tl_red, stops = self._history_queue[idx]
+            vehicles, walkers, tl_green, tl_yellow, tl_red, stops = self._history_queue[
+                idx
+            ]
 
             vehicle_masks.append(self._get_mask_from_actor_list(vehicles, M_warp))
             walker_masks.append(self._get_mask_from_actor_list(walkers, M_warp))
@@ -227,7 +298,14 @@ class ObsManager(ObsManagerBase):
             tl_red_masks.append(self._get_mask_from_stopline_vtx(tl_red, M_warp))
             stop_masks.append(self._get_mask_from_actor_list(stops, M_warp))
 
-        return vehicle_masks, walker_masks, tl_green_masks, tl_yellow_masks, tl_red_masks, stop_masks
+        return (
+            vehicle_masks,
+            walker_masks,
+            tl_green_masks,
+            tl_yellow_masks,
+            tl_red_masks,
+            stop_masks,
+        )
 
     def _get_mask_from_stopline_vtx(self, stopline_vtx, M_warp):
         mask = np.zeros([self._width, self._width], dtype=np.uint8)
@@ -237,25 +315,31 @@ class ObsManager(ObsManagerBase):
             point_1 = int(stopline_warped[0, 0, 0]), int(stopline_warped[0, 0, 1])
             point_2 = int(stopline_warped[1, 0, 0]), int(stopline_warped[1, 0, 1])
             cv.line(mask, point_1, point_2, color=1, thickness=6)
-        return mask.astype(np.bool)
+        # return mask.astype(np.bool)
+        return mask.astype(np.bool_)
 
     def _get_mask_from_actor_list(self, actor_list, M_warp):
         mask = np.zeros([self._width, self._width], dtype=np.uint8)
         for actor_transform, bb_loc, bb_ext in actor_list:
 
-            corners = [carla.Location(x=-bb_ext.x, y=-bb_ext.y),
-                       carla.Location(x=bb_ext.x, y=-bb_ext.y),
-                       carla.Location(x=bb_ext.x, y=0),
-                       carla.Location(x=bb_ext.x, y=bb_ext.y),
-                       carla.Location(x=-bb_ext.x, y=bb_ext.y)]
+            corners = [
+                carla.Location(x=-bb_ext.x, y=-bb_ext.y),
+                carla.Location(x=bb_ext.x, y=-bb_ext.y),
+                carla.Location(x=bb_ext.x, y=0),
+                carla.Location(x=bb_ext.x, y=bb_ext.y),
+                carla.Location(x=-bb_ext.x, y=bb_ext.y),
+            ]
             corners = [bb_loc + corner for corner in corners]
 
             corners = [actor_transform.transform(corner) for corner in corners]
-            corners_in_pixel = np.array([[self._world_to_pixel(corner)] for corner in corners])
+            corners_in_pixel = np.array(
+                [[self._world_to_pixel(corner)] for corner in corners]
+            )
             corners_warped = cv.transform(corners_in_pixel, M_warp)
 
             cv.fillConvexPoly(mask, np.round(corners_warped).astype(np.int32), 1)
-        return mask.astype(np.bool)
+        # return mask.astype(np.bool)
+        return mask.astype(np.bool_)
 
     @staticmethod
     def _get_surrounding_actors(bbox_list, criterium, scale=None):
@@ -270,7 +354,9 @@ class ObsManager(ObsManagerBase):
                     bb_ext.x = max(bb_ext.x, 0.8)
                     bb_ext.y = max(bb_ext.y, 0.8)
 
-                actors.append((carla.Transform(bbox.location, bbox.rotation), bb_loc, bb_ext))
+                actors.append(
+                    (carla.Transform(bbox.location, bbox.rotation), bb_loc, bb_ext)
+                )
         return actors
 
     def _get_warp_transform(self, ev_loc, ev_rot):
@@ -278,16 +364,30 @@ class ObsManager(ObsManagerBase):
         yaw = np.deg2rad(ev_rot.yaw)
 
         forward_vec = np.array([np.cos(yaw), np.sin(yaw)])
-        right_vec = np.array([np.cos(yaw + 0.5*np.pi), np.sin(yaw + 0.5*np.pi)])
+        right_vec = np.array([np.cos(yaw + 0.5 * np.pi), np.sin(yaw + 0.5 * np.pi)])
 
-        bottom_left = ev_loc_in_px - self._pixels_ev_to_bottom * forward_vec - (0.5*self._width) * right_vec
-        top_left = ev_loc_in_px + (self._width-self._pixels_ev_to_bottom) * forward_vec - (0.5*self._width) * right_vec
-        top_right = ev_loc_in_px + (self._width-self._pixels_ev_to_bottom) * forward_vec + (0.5*self._width) * right_vec
+        bottom_left = (
+            ev_loc_in_px
+            - self._pixels_ev_to_bottom * forward_vec
+            - (0.5 * self._width) * right_vec
+        )
+        top_left = (
+            ev_loc_in_px
+            + (self._width - self._pixels_ev_to_bottom) * forward_vec
+            - (0.5 * self._width) * right_vec
+        )
+        top_right = (
+            ev_loc_in_px
+            + (self._width - self._pixels_ev_to_bottom) * forward_vec
+            + (0.5 * self._width) * right_vec
+        )
 
-        src_pts = np.stack((bottom_left, top_left, top_right), axis=0).astype(np.float32)
-        dst_pts = np.array([[0, self._width-1],
-                            [0, 0],
-                            [self._width-1, 0]], dtype=np.float32)
+        src_pts = np.stack((bottom_left, top_left, top_right), axis=0).astype(
+            np.float32
+        )
+        dst_pts = np.array(
+            [[0, self._width - 1], [0, 0], [self._width - 1, 0]], dtype=np.float32
+        )
         return cv.getAffineTransform(src_pts, dst_pts)
 
     def _world_to_pixel(self, location, projective=False):
